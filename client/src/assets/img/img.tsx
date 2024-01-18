ff858d1b737466cb1d75e18e7e7c2b12cd2f895b
Jörg <hello@joerg-leidenberger.com>
2024-01-18 08:40:26 +0100

    chore: add images

---
+++ client/src/assets/img/img.tsx
@@ -0,0 +1,29 @@
+import add_task from './add_task.svg';
+import board from './board.svg';
+import calendar from './calendar.svg';
+import help from './help.svg';
+import logo from './logo.svg';
+import summary from './summary.svg';
+import backlog from './backlog.png';
+import boardsummery from './boardsummery.png';
+import done from './done.png';
+import feedback from './feedback.png';
+import progress from './progress.png';
+import todo from './todo.png';
+import urgent from './urgent.png';
+
+export {
+  add_task,
+  board,
+  calendar,
+  help,
+  logo,
+  summary,
+  backlog,
+  boardsummery,
+  done,
+  feedback,
+  progress,
+  todo,
+  urgent,
+};
