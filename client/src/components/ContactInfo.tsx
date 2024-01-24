3edc41aff87bbc453589a0c4dedb3ef98ffe25ce
Jörg <hello@joerg-leidenberger.com>
2024-01-24 19:05:58 +0100

    feat: create contactInfo layout

---
+++ client/src/components/ContactInfo.tsx
@@ -0,0 +1,36 @@
+import { deleteIcon, editIcon } from '../assets/img/img';
+
+function ContactInfo() {
+  return (
+    <div className='contactInfo'>
+      <div className='contactInfo__container'>
+        <div className='contactInfo__header'>
+          <div className='contactInfo__initials'>AM</div>
+          <div className='contactInfo__name'>
+            <div>Anton Mayer</div>
+            <div>
+              <span>
+                <img src={editIcon} alt='edit icon' /> Edit
+              </span>
+              <span>
+                <img src={deleteIcon} alt='delete icon' /> Delete
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className='contactInfo__title'>Contact Information</div>
+        <div className='contactInfo__infoBox'>
+          <div>
+            <span>Email</span>
+            <span className='contactInfo__email'>antom@gmail.com</span>
+          </div>
+          <div>
+            <span>Phone</span>
+            <span>+49 1111 111 11 1</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ContactInfo;
--- client/src/pages/Contacts.tsx
+++ client/src/pages/Contacts.tsx
@@ -1,9 +1,11 @@
 import ContactList from '../components/ContactList';
+import ContactInfo from '../components/ContactInfo';
 
 function Contacts() {
   return (
     <div className='contacts'>
       <ContactList />
+      <ContactInfo />
     </div>
   );
 }
