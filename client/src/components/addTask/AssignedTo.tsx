3a2f787d0529e8ce31eee72acc4798c77059ca17
Jörg <hello@joerg-leidenberger.com>
2024-01-30 16:26:33 +0100

    feat: add showContactList onClick

--- client/src/components/addTask/AssignedTo.tsx
+++ client/src/components/addTask/AssignedTo.tsx
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import { arrowDropDown, checkButton } from '../../assets/img/img';
 
 function AssignedTo() {
+  const [showContactList, setShowContactList] = useState(false);
+  const toggleContactList = () => {
+    setShowContactList(!showContactList);
+  };
+
   return (
     <label>
       <div className='assignedTo'>
         <div className='addTask__title'>
           Assigned to <span>(optional)</span>
         </div>
-        <div className='assignedTo__inputContainer'>
+        <div className='assignedTo__inputContainer' onClick={toggleContactList}>
           <input
             className='assignedTo__input'
             type='text'
@@ -18,89 +24,91 @@ function AssignedTo() {
             <img src={arrowDropDown} alt='arrow icon' />
           </span>
         </div>
-        <div className='assignedTo__contactList'>
-          <div>
-            <ul>
-              <li>
-                <div>
-                  <div className='assignedTo__contactInitials'>AM</div>
-                  <span className='assignedTo__contactName'>Anton Mayer</span>
-                </div>
-                <img
-                  className='assignedTo__checkbox'
-                  src={checkButton}
-                  alt='checkbox icon'
-                />
-              </li>
-              <li>
-                <div>
-                  <div className='assignedTo__contactInitials'>AM</div>
-                  <span className='assignedTo__contactName'>Anton Mayer</span>
-                </div>
-                <img
-                  className='assignedTo__checkbox'
-                  src={checkButton}
-                  alt='checkbox icon'
-                />
-              </li>
-              <li>
-                <div>
-                  <div className='assignedTo__contactInitials'>AM</div>
-                  <span className='assignedTo__contactName'>Anton Mayer</span>
-                </div>
-                <img
-                  className='assignedTo__checkbox'
-                  src={checkButton}
-                  alt='checkbox icon'
-                />
-              </li>
-              <li>
-                <div>
-                  <div className='assignedTo__contactInitials'>AM</div>
-                  <span className='assignedTo__contactName'>Anton Mayer</span>
-                </div>
-                <img
-                  className='assignedTo__checkbox'
-                  src={checkButton}
-                  alt='checkbox icon'
-                />
-              </li>
-              <li>
-                <div>
-                  <div className='assignedTo__contactInitials'>AM</div>
-                  <span className='assignedTo__contactName'>Anton Mayer</span>
-                </div>
-                <img
-                  className='assignedTo__checkbox'
-                  src={checkButton}
-                  alt='checkbox icon'
-                />
-              </li>
-              <li>
-                <div>
-                  <div className='assignedTo__contactInitials'>AM</div>
-                  <span className='assignedTo__contactName'>Anton Mayer</span>
-                </div>
-                <img
-                  className='assignedTo__checkbox'
-                  src={checkButton}
-                  alt='checkbox icon'
-                />
-              </li>
-              <li>
-                <div>
-                  <div className='assignedTo__contactInitials'>AM</div>
-                  <span className='assignedTo__contactName'>Anton Mayer</span>
-                </div>
-                <img
-                  className='assignedTo__checkbox'
-                  src={checkButton}
-                  alt='checkbox icon'
-                />
-              </li>
-            </ul>
+        {showContactList && (
+          <div className='assignedTo__contactList'>
+            <div>
+              <ul>
+                <li>
+                  <div>
+                    <div className='assignedTo__contactInitials'>AM</div>
+                    <span className='assignedTo__contactName'>Anton Mayer</span>
+                  </div>
+                  <img
+                    className='assignedTo__checkbox'
+                    src={checkButton}
+                    alt='checkbox icon'
+                  />
+                </li>
+                <li>
+                  <div>
+                    <div className='assignedTo__contactInitials'>AM</div>
+                    <span className='assignedTo__contactName'>Anton Mayer</span>
+                  </div>
+                  <img
+                    className='assignedTo__checkbox'
+                    src={checkButton}
+                    alt='checkbox icon'
+                  />
+                </li>
+                <li>
+                  <div>
+                    <div className='assignedTo__contactInitials'>AM</div>
+                    <span className='assignedTo__contactName'>Anton Mayer</span>
+                  </div>
+                  <img
+                    className='assignedTo__checkbox'
+                    src={checkButton}
+                    alt='checkbox icon'
+                  />
+                </li>
+                <li>
+                  <div>
+                    <div className='assignedTo__contactInitials'>AM</div>
+                    <span className='assignedTo__contactName'>Anton Mayer</span>
+                  </div>
+                  <img
+                    className='assignedTo__checkbox'
+                    src={checkButton}
+                    alt='checkbox icon'
+                  />
+                </li>
+                <li>
+                  <div>
+                    <div className='assignedTo__contactInitials'>AM</div>
+                    <span className='assignedTo__contactName'>Anton Mayer</span>
+                  </div>
+                  <img
+                    className='assignedTo__checkbox'
+                    src={checkButton}
+                    alt='checkbox icon'
+                  />
+                </li>
+                <li>
+                  <div>
+                    <div className='assignedTo__contactInitials'>AM</div>
+                    <span className='assignedTo__contactName'>Anton Mayer</span>
+                  </div>
+                  <img
+                    className='assignedTo__checkbox'
+                    src={checkButton}
+                    alt='checkbox icon'
+                  />
+                </li>
+                <li>
+                  <div>
+                    <div className='assignedTo__contactInitials'>AM</div>
+                    <span className='assignedTo__contactName'>Anton Mayer</span>
+                  </div>
+                  <img
+                    className='assignedTo__checkbox'
+                    src={checkButton}
+                    alt='checkbox icon'
+                  />
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </label>
   );
