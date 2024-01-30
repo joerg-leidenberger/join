fa3ee1dcabb497f7fbdd1c7bf13f0e90288286c3
Jörg <hello@joerg-leidenberger.com>
2024-01-30 18:22:35 +0100

    feat(assignetTo): add handle input

--- client/src/components/addTask/AssignedTo.tsx
+++ client/src/components/addTask/AssignedTo.tsx
@@ -1,10 +1,30 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { arrowDropDown, checkButton } from '../../assets/img/img';
 
 function AssignedTo() {
+  const [assignedToInputValue, setAssignedToInputValue] = useState('');
   const [showContactList, setShowContactList] = useState(false);
+  const [placeholder, setPlaceholder] = useState('Select contacts to assign');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAssignedToInputValue(e.target.value);
+  };
+
+  const handleInputValue = () => {
+    if (showContactList) {
+      setAssignedToInputValue('');
+      (document.activeElement as HTMLInputElement).blur();
+      setPlaceholder('Select contacts to assign');
+    }
+  };
+
   const toggleContactList = () => {
     setShowContactList(!showContactList);
+  };
+
+  const openContactList = () => {
+    setShowContactList(true);
+    setPlaceholder('');
   };
 
   return (
@@ -13,14 +33,23 @@ function AssignedTo() {
         <div className='addTask__title'>
           Assigned to <span>(optional)</span>
         </div>
-        <div className='assignedTo__inputContainer' onClick={toggleContactList}>
+        <div className='assignedTo__inputContainer'>
           <input
             className='assignedTo__input'
             type='text'
             id='assignedTo'
-            placeholder='Select contacts to assign'
+            placeholder={placeholder}
+            onClick={openContactList}
+            onChange={handleInputChange}
+            value={assignedToInputValue}
           ></input>
-          <span className='assignedTo__arrowIcon'>
+          <span
+            className='assignedTo__arrowIcon'
+            onClick={() => {
+              toggleContactList();
+              handleInputValue();
+            }}
+          >
             <img src={arrowDropDown} alt='arrow icon' />
           </span>
         </div>
