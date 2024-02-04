3d6c77d21ce972298a92317bc4bf0893f885c89d
Jörg <hello@joerg-leidenberger.com>
2024-02-04 04:18:19 +0100

    feat: layout for login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,4 +1,37 @@
+import { joinSVG } from '../assets/img/img';
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className='login'>
+      <div className='login__head'>
+        <div className='login__logo'>{joinSVG()}</div>
+        <div className='login__signUp'>
+          <div>Not a Join user?</div> <button>Sign up</button>
+        </div>
+      </div>
+      <div className='login__body'>
+        <div className='login__headline'>Log in</div>
+        <div className='login__form'>
+          <input type='text' placeholder='Email' />
+          <input type='text' placeholder='Password' />
+          <input
+            className='login__form__checkbox'
+            id='checkbox'
+            type='checkbox'
+          />
+          <label htmlFor='checkbox'>Remember me</label>
+          <div className='login__form__buttons'>
+            <button className='login__form__buttons-login'>Login</button>
+            <button className='login__form__buttons-guestLogin'>
+              Guest log in
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className='login__footer'>
+        <a href='#'>Privacy Policy</a> <a href='#'>Privacy Policy</a>
+      </div>
+    </div>
+  );
 }
 export default Login;
